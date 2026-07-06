e836d00fa76ac652f2bd9b415c7ae9a3f733db91
thrashman2015 <xavierthrasher2015@gmail.com>
2026-07-06 15:48:49 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 . 3 3 
+. 3 3 3 3 3 3 3 3 3 3 . . . . . 
+. 3 3 3 3 3 3 3 . . . . . . . . 
+. 3 3 3 3 3 3 3 3 3 3 . . . . . 
+3 3 3 3 3 3 3 3 3 3 . . . . . . 
+3 3 3 3 3 3 3 3 3 3 . . . . . . 
+3 3 . 3 3 3 3 3 . . . . . . . . 
+. . . . 3 3 3 3 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
